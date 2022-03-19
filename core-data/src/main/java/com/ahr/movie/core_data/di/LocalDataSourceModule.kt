edc0b5f651743f7e89c4972d0a7a1879a65731b7
Abdul Hafiz Ramadan <abdulhafizr>
2022-03-19 20:01:35 +0700

    | Implement database encryption

--- core-data/src/main/java/com/ahr/movie/core_data/di/LocalDataSourceModule.kt
+++ core-data/src/main/java/com/ahr/movie/core_data/di/LocalDataSourceModule.kt
@@ -2,6 +2,7 @@ package com.ahr.movie.core_data.di
 
 import android.content.Context
 import androidx.room.Room
+import androidx.sqlite.db.SupportSQLiteOpenHelper
 import com.ahr.movie.core_data.local.daos.MovieDao
 import com.ahr.movie.core_data.local.databases.MovieDatabase
 import dagger.Module
@@ -9,6 +10,8 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import javax.inject.Singleton
 
 @Module
@@ -17,14 +20,25 @@ object LocalDataSourceModule {
 
     @Provides
     @Singleton
+    @SQLCipherFactory
+    fun provideSQLCipherFactory(): SupportSQLiteOpenHelper.Factory {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("movie".toCharArray())
+        return SupportFactory(passphrase)
+    }
+
+    @Provides
+    @Singleton
     fun provideMovieDatabase(
-        @ApplicationContext context: Context
+        @ApplicationContext context: Context,
+        @SQLCipherFactory factory: SupportSQLiteOpenHelper.Factory
     ): MovieDatabase =
         Room.databaseBuilder(
             context,
             MovieDatabase::class.java,
             "movie-database"
-        ).build()
+        )
+        .openHelperFactory(factory)
+        .build()
 
     @Provides
     @Singleton
